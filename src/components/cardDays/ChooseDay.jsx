b637bd2bd49605c40e20f01f8cf150b6302e8422
Eugenia Saravia <63812926+Euge-Saravia@users.noreply.github.com>
2024-09-08 19:12:32 +0200

    Configuration of UseApi to get menus

--- src/components/cardDays/ChooseDay.jsx
+++ src/components/cardDays/ChooseDay.jsx
@@ -1,23 +1,39 @@
 import "./chooseDay.scss";
 import PropTypes from "prop-types";
-import MealType from "../mealtype/MealType";
-import { Link } from "react-router-dom";
+// import MealType from "../mealtype/MealType";
+// import { Link } from "react-router-dom";
 
-const ChooseDay = ({ day }) => {
+const ChooseDay = ({ day, breakfast, lunch, dinner }) => {
   return (
     <div className="containerChooseDay">
       <h5 className="dayOfWeek">{day}</h5>
-      <Link to="/addPlate">
+      <div className="meal">
+        <strong>Breakfast:</strong> {breakfast?.plate.name || "No meal planned"}
+      </div>
+      <div className="meal">
+        <strong>Lunch:</strong> {lunch?.plate.name || "No meal planned"}
+      </div>
+      <div className="meal">
+        <strong>Dinner:</strong> {dinner?.plate.name || "No meal planned"}
+      </div>
+      {/* <Link to="/addPlate">
         <MealType type="Breakfast" />
       </Link>
       <MealType type="Lunch" />
-      <MealType type="Dinner" />
+      <MealType type="Dinner" /> */}
     </div>
   );
 };
 
+// ChooseDay.propTypes = {
+//   day: PropTypes.string,
+// };
+
 ChooseDay.propTypes = {
-  day: PropTypes.string,
+  day: PropTypes.string.isRequired,
+  breakfast: PropTypes.object,
+  lunch: PropTypes.object,
+  dinner: PropTypes.object,
 };
 
 export default ChooseDay;
