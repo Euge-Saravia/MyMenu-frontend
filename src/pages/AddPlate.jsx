95206a528e9a2f0607d331dc99357bfa5bbe754f
Eugenia Saravia <63812926+Euge-Saravia@users.noreply.github.com>
2024-09-08 13:00:58 +0200

    Installing library for days and weeks, create route to addPlate

---
+++ src/pages/AddPlate.jsx
@@ -0,0 +1,12 @@
+
+import HomeAddAPlate from '../views/HomeAddAPlate'
+
+const AddPlate = () => {
+  return (
+    <div>
+        <HomeAddAPlate />
+    </div>
+  )
+}
+
+export default AddPlate
---
+++ src/views/HomeAddAPlate.jsx
@@ -0,0 +1,11 @@
+import Field from "../components/labels/Field"
+
+const HomeAddAPlate = () => {
+  return (
+    <div>
+      <Field />
+    </div>
+  )
+}
+
+export default HomeAddAPlate
